892d6fb07fb2ac22628f176b654bb4c10d79b23f
Maxim Klechikov <maximkakl1@gmail.com>
2023-04-01 17:32:40 +0300

    rename directory 'tests' to '__tests__'

--- tests/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import { strict as assert } from 'node:assert';
-import { capitalize } from "../src/capitalize.js";
-
-assert.deepEqual(capitalize('str'), 'Str');
-assert.deepEqual(capitalize(''), '');
-
-console.log('Все тесты пройдены!');
